074c855a994a912b90767d15b12efb3c64f52a79
atul030702 <singhatul0307@gmail.com>
2025-07-20 20:09:52 +0530

    bubble sort with optimisation

---
+++ Sorting Algoritm/Basic/bubbleSort.js
@@ -0,0 +1,23 @@
+// Implementing bubble sort O(n^2)
+
+function bubbleSort(arr) {
+    for(let i = 0; i < arr.length - 1; i++) {
+
+        let isSwapped = false; // To check whether swapping is being done or not
+
+        for(let j = 0; j < (arr.length - 1) - i; j++) {
+
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                isSwapped = true; // true means, swapping is being done
+            }
+            console.log(arr);
+        }
+        if(!isSwapped) break; // If it is false then break the loop, array is already sorted
+    }
+}
+
+bubbleSort([0, 1, 5, 8, 2, 3]);
